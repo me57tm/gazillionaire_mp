4d289a1e024b7df875c917adf83eb3784196194c
me57tm <jonathancaines1@gmail.com>
2024-08-04 02:11:04 +0100

    progress on testing ways to do things and the game controller

--- game_controller.py
+++ game_controller.py
@@ -1,4 +1,11 @@
+from pywinauto import Application, keyboard
+
+app = Application().connect(path='Gazillionaire.exe')
+GAME = app.Gazillionaire
+GAME.move_window(x=None, y=None, width=800, height=627)
+
 class Menu:
+    global GAME
     commands = []
     next_menu = None
 
@@ -13,22 +20,71 @@ class Menu:
     def execute(self, command, args):
         pass
 
+    def moneyMenu(self,amount):
+        #stocks upper not the same
+        if amount == "max":
+            GAME.click_input(coords=(460, 370))
+        else:
+            try:
+                int(amount)
+                keyboard.send_keys(amount)
+            except ValueError:
+                pass
+        GAME.click_input(coords=(400, 435))
+
     def __str__(self):
         return self.commands
 
 class MainMenu(Menu):
     commands = [
-        "marketplace", "supply","warehouse"
+        "marketplace","market", "supply","warehouse"
         "passengers",
+        "pick",
+        "passenger",
         "advertise",
+        "ad",
+        "advert",
         "pay",
         "buy",
         "explore",
         "stocks",
+        "stock,"
         "bank",
         "loan",
         "zinn",
+        "zinns",
         "journey",
         "travel",
         "leave",
     ]
+    def execute(self, command, args):
+        match command:
+            case "marketplace" | "market":
+                GAME.click_input(coords=(350, 150))
+            case "supply":
+                pass
+            case "warehouse":
+                pass
+            case "journey" | "travel" | "leave":
+                GAME.click_input(coords=(150, 150))
+            case "stock" | "stocks":
+                GAME.click_input(coords=(200, 350))
+            # case "money:
+                # game.click_input(coords=(200, 400))
+            case "bank":
+                GAME.click_input(coords=(200, 450))
+            case "loan":
+                GAME.click_input(coords=(200, 500))
+            case "zinn" | "zinns":
+                GAME.click_input(coords=(200, 550))
+            case "pick" | "passenger" | "passengers":
+                GAME.click_input(coords=(500, 250))
+            case "advert" | "advertise" | "ad":
+                GAME.click_input(coords=(500, 300))
+            case "pay":
+                GAME.click_input(coords=(200, 350))
+                GAME.click_input(coords=(200, 400))
+            case "buy":
+                GAME.click_input(coords=(200, 450))
+            case "explore":
+                GAME.click_input(coords=(200, 500))
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # This is a sample Python script.
+import pyautogui
+
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -18,8 +20,30 @@ if __name__ == '__main__':
 
 import pywinauto
 
-'''app = pywinauto.Application().connect(path='Gazillionaire.exe')
-#app.print_control_identifiers()
-app.ApolloRuntimeContentWindow.print_control_identifiers()
-app.Gazillionaire.click_input(coords=(100, 100))'''
+app = pywinauto.Application().connect(path='Gazillionaire.exe')
+game = app.Gazillionaire
+game.move_window(x=None, y=None, width=776, height=609)
+#Game window (Inc. Title Bar) - 760 * 600
+#Game window (Exc. Title Bar) - 760 * 570
+try:
+    location = pyautogui.locateOnScreen('planets/mira.png')
+    pyautogui.click(location)
+except:
+    pass
 
+x = ""
+while x != "quit":
+    x = input(">>>")
+    if x == "ss":
+        game.capture_as_image().save('screenshot.png')
+    elif x == "x":
+        try:
+            location = pyautogui.locateOnScreen('x.png')
+            pyautogui.click(location)
+        except pyautogui.ImageNotFoundException:
+            pass
+    else:
+        x = x.split(" ")
+        x[0] = int(x[0])
+        x[1] = int(x[1])
+        game.click_input(coords=(x[0], x[1]))
